0953b4f1e04fdf00d67145d788bd36350d254c26
mirian228 <pariksini@mail.ru>
2023-06-13 14:00:58 +0400

    Arithmetic Operations

---
+++ arithmetic operations/arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+    // Arithmetic operators
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+    $counter = 0;
+
+   // $z = $x + $y;
+   // $z = $x - $y;
+   // $z = $x * $y;
+   // $z = $x / $y;
+   // $z = $x ** $y;
+   // $z = $x % $y;
+   // echo "{$z}";
+    
+   // $counter++5;
+   // $counter--5;
+   // $counter+=5;
+   // $counter-=5;
+   echo "{$counter}";
+
+   // Operator precedence
+   // ()
+   // **
+   // * / %
+   // +-
+
+   $total = 1 +2 - 3 * 4 / 5 ** 6;
+   echo $total;
+?>
